b697440d1730c10293f3b89b85d704934e26a626
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:24:08 -0500

    Add ProyectoClas0ApplicationTests for testing the new application structure

---
+++ src/test/java/com/ejemplo_poo/EjemploPooApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto_cl.proyecto_clase0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoClas0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
