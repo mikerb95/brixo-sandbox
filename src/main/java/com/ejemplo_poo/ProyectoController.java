99058b4e84f9f9e2b458d7c3cb74ec1333c94f0a
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:18:47 -0500

    Remove EjemploPooController and add ProyectoController with the same functionality

--- src/main/java/com/ejemplo_poo/ProyectoController.java
+++ src/main/java/com/ejemplo_poo/ProyectoController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EjemploPooController {
+public class ProyectoController {
     @GetMapping("/proyecto")
     public String proyecto(){
         return "Proyecto Ejemplo POO";
