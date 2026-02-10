8bbddce2f9c13ff41aa08e1b8956d2d4124771a9
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:44:41 -0500

    Fix Proyectocontroller endpoint mapping to root URL

--- src/main/java/proyecto_cl/proyecto_clase0/proyectocontroller.java
+++ src/main/java/proyecto_cl/proyecto_clase0/proyectocontroller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class proyectocontroller {
-    @RequestMapping("/ proyecto Clas0")
+    @RequestMapping("/")
     public String proyecto() {
         return "hola mundo";
     }
