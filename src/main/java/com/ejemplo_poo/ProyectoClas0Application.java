022621ba77c92ae48cd2263b12ccc339e831deea
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:22:43 -0500

    Refactor project structure: update groupId and artifactId in pom.xml; replace EjemploPooApplication with ProyectoClas0Application; rename EjemploPooController to Proyectocontroller

--- src/main/java/com/ejemplo_poo/ProyectoClas0Application.java
+++ src/main/java/com/ejemplo_poo/ProyectoClas0Application.java
@@ -1,13 +1,13 @@
-package com.ejemplo_poo;
+package proyecto_cl.proyecto_clase0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EjemploPooApplication {
+public class ProyectoClas0Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EjemploPooApplication.class, args);
+		SpringApplication.run(ProyectoClas0Application.class, args);
 	}
 
 }
