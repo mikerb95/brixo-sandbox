3f0272bdb37925a723bb71b870502cb88ff6706c
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:26:40 -0500

    Fix POM configuration and update Proyectocontroller package structure and endpoint

--- src/main/java/proyecto_cl/proyecto_clase0/proyectocontroller.java
+++ src/main/java/proyecto_cl/proyecto_clase0/proyectocontroller.java
@@ -1,12 +1,11 @@
-package proyecto_cl;
+package proyecto_cl.proyecto_clase0;
 
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class proyectocontroller {
-    @RequestMapping("/ proyecto Clas0")
+    @RequestMapping("/")
     public String proyecto() {
         return "hola mundo";
     }
