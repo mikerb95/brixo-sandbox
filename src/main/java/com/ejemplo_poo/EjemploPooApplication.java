00e82e5876c24af7130fa8b2d54d681d20c52591
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:22:54 -0500

    Add ProyectoClas0Application and Proyectocontroller with /proyecto endpoint

---
+++ src/main/java/com/ejemplo_poo/EjemploPooApplication.java
@@ -0,0 +1,13 @@
+package proyecto_cl.proyecto_clase0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoClas0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoClas0Application.class, args);
+	}
+
+}
