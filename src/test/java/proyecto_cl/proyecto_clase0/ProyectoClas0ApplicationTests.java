d6b813ed9339c5a3a481357e871e7aa08fd7e97f
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:24:01 -0500

    Refactor project structure: remove EjemploPooApplication, EjemploPooController, and related tests; add ProyectoClas0Application, Proyectocontroller, and corresponding tests

--- src/test/java/proyecto_cl/proyecto_clase0/ProyectoClas0ApplicationTests.java
+++ src/test/java/proyecto_cl/proyecto_clase0/ProyectoClas0ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ejemplo_poo;
+package proyecto_cl.proyecto_clase0;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EjemploPooApplicationTests {
+class ProyectoClas0ApplicationTests {
 
 	@Test
 	void contextLoads() {
